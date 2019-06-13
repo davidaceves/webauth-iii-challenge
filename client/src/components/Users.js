e31e645e8fa394ccbc5348b48c3ffbb35da9d794
David <digital0david@gmail.com>
2019-06-12 22:56:26 -0400

    users

--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -1,10 +1,32 @@
 import React from "react";
+import api from "../helpers/api.js";
 
 class Users extends React.Component {
+  state = {
+    users: []
+  };
+
+  async componentDidMount() {
+    try {
+      const result = await api.get("/users");
+
+      this.setState({
+        users: result.data
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
   render() {
     return (
       <>
         <h3>Users</h3>
+
+        <ul>
+          {this.state.users.map((user, i) => {
+            return <li key={i}>{user.username}</li>;
+          })}
+        </ul>
       </>
     );
   }
--- client/src/helpers/api.js
+++ client/src/helpers/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "http://localhost:5000/api",
+  headers: {
+    Authorization: localStorage.getItem("token")
+  }
 });
 
 export default instance;
